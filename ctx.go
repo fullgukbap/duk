efdb83f5e03f4edbc6dbc752f3d32a768ef29777
fullgukbap <2hanjunbum6@gmail.com>
2024-06-07 15:47:31 +0900

    refactor: add new, handlerfunc

--- ctx.go
+++ ctx.go
@@ -2,8 +2,16 @@ package duk
 
 import "encoding/json"
 
+type HandlerFunc func(*Ctx)
+
 type Ctx struct {
 	Payload string
+}
+
+func NewCtx(payload string) *Ctx {
+	return &Ctx{
+		Payload: payload,
+	}
 }
 
 func (c *Ctx) Parse(out any) error {
